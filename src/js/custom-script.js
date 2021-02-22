403cf6cb082a65c2f23a9e23ff4379abecb73950
Enamul Kabir <enamul.kabirseu@gmail.com>
2021-02-22 12:34:39 +0600

    UI Design

--- src/js/custom-script.js
+++ src/js/custom-script.js
@@ -27,6 +27,32 @@ jQuery(document).ready(function($) {
                 }
             }
         ]
+    });
+
+    $('.team-slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+            }
+        ]
     })
 });
 
